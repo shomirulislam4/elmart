46c138a9f8f30eef967980b8c1e1b9f990384ccb
shomirulislam4 <shomirulislam4@gmail.com>
2024-05-16 00:09:32 +0600

    new demo

---
+++ demo/fitness-equipment/js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("body").prepend('<div class="cursor"></div>');
+    $(document).mousemove(function(e) {
+        $('.cursor').css({
+            "left": e.pageX,
+            "top": e.pageY - $(window).scrollTop()
+        });
+    });
+    $("a, button, div, span, input[type=submit]").mouseenter(function(){
+        var cursorStyle = $(this).css('cursor');
+        if (cursorStyle === 'pointer') {
+            $('.cursor').addClass('hover');
+        }
+    }).mouseleave(function(){
+        $('.cursor').removeClass('hover');
+    });
+});
