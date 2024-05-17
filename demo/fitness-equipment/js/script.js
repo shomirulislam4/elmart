78bd19ad77a6c4fa8b15bf28e6993acf21943ee3
shomirulislam4 <shomirulislam4@gmail.com>
2024-05-17 21:10:50 +0600

    update

--- demo/fitness-equipment/js/script.js
+++ demo/fitness-equipment/js/script.js
@@ -16,3 +16,23 @@ $(function(){
         $('.cursor').removeClass('hover');
     });
 });
+
+//Cart Slider
+$(function(){
+    $('.fes__header__cart').on('click', function(){
+        $('.fes__cart__slider').addClass('show')
+    })
+    $('.fes__cart__closebtn').on('click', function(){
+        $('.fes__cart__slider').removeClass('show')
+    })
+})
+
+//Right Side Slider Menu
+$(function(){
+    $('.fes__rsm__btn').on('click', function(){
+        $('.fes__right__slider__menu').addClass('show')
+    })
+    $('.fes__rsm__closebtn').on('click', function(){
+        $('.fes__right__slider__menu').removeClass('show')
+    })
+})
