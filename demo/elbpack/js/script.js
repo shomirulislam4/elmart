d692f0374c092a580d4b7663c84d775567440644
shomirulislam4 <shomirulislam4@gmail.com>
2024-09-23 22:25:32 +0600

    new section

--- demo/elbpack/js/script.js
+++ demo/elbpack/js/script.js
@@ -246,7 +246,7 @@
         });
         
 
-    /*============================ Single Product Page ============================*/
+    /*============================ Shop & Single Product Page ============================*/
         //Single Product Image
         $(document).ready(function(){
             $('.elbpsmalimage').click(function() {
@@ -257,6 +257,50 @@
                 $(this).attr('src', bigImageSrc);
             });
         });
-        
 
+        //Price Filter
+        $(function(){
+            var rangeInput = document.querySelectorAll(".range-input input"),
+            priceInput = document.querySelectorAll(".price-input input"),
+            range = document.querySelector(".price-range-slider .progress");
+            let priceGap = 5;
+          
+            rangeInput.forEach(input =>{
+                input.addEventListener("input", e =>{
+                    let minVal = parseInt(rangeInput[0].value),
+                    maxVal = parseInt(rangeInput[1].value);
+          
+                    if((maxVal - minVal) < priceGap){
+                        if(e.target.className === "range-min"){
+                            rangeInput[0].value = maxVal - priceGap
+                        }else{
+                            rangeInput[1].value = minVal + priceGap;
+                        }
+                    }else{
+                        priceInput[0].value = minVal;
+                        priceInput[1].value = maxVal;
+                        range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+                        range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+                    }
+                });
+          
+                priceInput.forEach(input =>{
+                  input.addEventListener("input", e =>{
+                      let minPrice = parseInt(priceInput[0].value),
+                      maxPrice = parseInt(priceInput[1].value);
+                      
+                      if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+                          if(e.target.className === "input-min"){
+                              rangeInput[0].value = minPrice;
+                              range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+                          }else{
+                              rangeInput[1].value = maxPrice;
+                              range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+                          }
+                      }
+                  });
+                });
+            });
+          });
+          
 }) (jQuery);
